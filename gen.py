79ad002461c04858971001d3188bd5f802a77c94
Douglas Webster <douglaswebster98@gmail.com>
2020-09-23 14:45:54 -0700

    initial commit

---
+++ gen.py
@@ -0,0 +1,39 @@
+from Crypto.PublicKey import RSA
+import json
+
+def generate_keys():
+
+    # Generating key pair for Alice
+    alice_key = RSA.generate(2048)
+    alice_private_key = alice_key.export_key('PEM')
+    alice_public_key = alice_key.publickey().export_key('PEM')
+
+    # Generating key pair for Bob
+    bob_key = RSA.generate(2048)
+    bob_private_key = bob_key.export_key('PEM')
+    bob_public_key = bob_key.publickey().export_key('PEM')
+
+    # Writing Alice's keys
+    f = open('./keys/alice_priv.pem','wb')
+    f.write(alice_private_key)
+    f.close()
+    f = open('./keys/alice_public.pem','wb')
+    f.write(alice_public_key)
+    f.close()
+
+    # Writing Bob's keys
+    f = open('./keys/bob_priv.pem', 'wb')
+    f.write(bob_private_key)
+    f.close()
+    f = open('./keys/bob_public.pem', 'wb')
+    f.write(bob_public_key)
+    f.close()
+
+    f2 = open('./keys/alice_priv.pem','rb')
+    key = RSA.import_key(f2.read())
+    f2.close()
+
+    print(key.size_in_bits())
+
+generate_keys()
+
